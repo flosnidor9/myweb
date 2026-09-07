3d53dea0973244bb85e53ba0dc4976ad048f4cc7
flosnidor9 <flosnidor9@gmail.com>
2026-09-07 14:16:05 +0900

    보안규칙 및 로그인 admin 추가

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase 웹 앱 식별용 공개 설정입니다. 서비스 계정 키는 절대 이 파일에 넣지 마세요.
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAU4yx1_46xsfjE4AYtaWwuTVb5EqlETpc',
+  authDomain: 'flosnidor9-myweb.firebaseapp.com',
+  projectId: 'flosnidor9-myweb',
+  storageBucket: 'flosnidor9-myweb.firebasestorage.app',
+  messagingSenderId: '1072651822846',
+  appId: '1:1072651822846:web:dc1ef596cfb9333e7067af',
+  measurementId: 'G-E76KVFD56F',
+};
